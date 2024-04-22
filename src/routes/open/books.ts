0ce9fa6e81751335ebe15457bbae34cfae40b294
Zac A <andersenzb@gmail.com>
2024-04-22 14:52:42 -0700

    Added books router!

---
+++ src/routes/open/books.ts
@@ -0,0 +1,24 @@
+//express is the framework we're going to use to handle requests
+import express, { NextFunction, Request, Response, Router } from 'express';
+//Access the connection to Postgres Database
+import { pool, validationFunctions } from '../../core/utilities';
+
+const bookRouter: Router = express.Router();
+
+bookRouter.get('/all',
+    (req: Request, res: Response, next: NextFunction) => {
+    const query = 'SELECT title, authors, publication_year, isbn13 FROM books';
+
+    pool.query(query)
+        .then((result) => {
+            res.send({
+                entries: result.rows
+            })
+        })
+        .catch((e) => {
+            console.log(`Server failed to retrieve books due to ${e}`)
+            res.status(500).send("Server Error, so sorry!")
+        })
+    })
+
+export {bookRouter}
