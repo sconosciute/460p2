398b3bb87d61f172286e246c2774d3f219dc6ea9
Zac A <andersenzb@gmail.com>
2024-04-29 17:08:39 -0700

    WIP upgrade script

--- migrations/up1.sql
+++ migrations/up1.sql
@@ -1,19 +1,27 @@
+CREATE TABLE IF NOT EXISTS schema_version
+(
+    version      SMALLINT,
+    upgrade_date TIMESTAMP
+);
+
+TRUNCATE schema_version;
+INSERT INTO schema_version VALUES (1, CURRENT_TIMESTAMP);
+
 CREATE TABLE IF NOT EXISTS roles
 (
     id           SERIAL PRIMARY KEY,
     role_name    TEXT    NOT NULL,
     admin        BOOLEAN NOT NULL,
-    comment      BOOLEAN NOT NULL,
     update_add   BOOLEAN NOT NULL,
     delete       BOOLEAN NOT NULL,
     manage_users BOOLEAN NOT NULL
 );
 
-TRUNCATE account CASCADE ;
+TRUNCATE account CASCADE;
 ALTER TABLE account
     DROP COLUMN IF EXISTS account_role;
 ALTER TABLE account
-    ADD COLUMN IF NOT EXISTS role_id INTEGER NOT NULL REFERENCES roles(id);
+    ADD COLUMN IF NOT EXISTS role_id INTEGER NOT NULL REFERENCES roles (id);
 
 CREATE TABLE IF NOT EXISTS authors
 (
@@ -24,19 +32,21 @@ CREATE TABLE IF NOT EXISTS authors
 CREATE TABLE IF NOT EXISTS book_author
 (
     id     SERIAL PRIMARY KEY,
-    book   INTEGER NOT NULL REFERENCES books(id),
-    author INTEGER NOT NULL REFERENCES authors(id)
+    book   INTEGER NOT NULL REFERENCES books (id),
+    author INTEGER NOT NULL REFERENCES authors (id)
 );
 
-ALTER TABLE book_author DROP CONSTRAINT IF EXISTS book_author_unique;
-ALTER TABLE book_author ADD CONSTRAINT book_author_unique UNIQUE (book, author);
+ALTER TABLE book_author
+    DROP CONSTRAINT IF EXISTS book_author_unique;
+ALTER TABLE book_author
+    ADD CONSTRAINT book_author_unique UNIQUE (book, author);
 
 do
 $$
     DECLARE
-        book       books%rowtype;
+        book        books%rowtype;
         author_name text;
-        author_id integer;
+        author_id   integer;
     BEGIN
         FOR book IN SELECT * FROM books
             LOOP
@@ -50,6 +60,7 @@ $$
                         INSERT INTO book_author (book, author) VALUES (book.id, author_id) ON CONFLICT DO NOTHING;
                     END LOOP;
             END LOOP;
-        ALTER TABLE books DROP COLUMN authors;
+        ALTER TABLE books
+            DROP COLUMN authors;
     END;
 $$
