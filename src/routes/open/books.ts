f0147e7e6f48ac8f8fef8c2dd42102b600ec93a3
Zac <andersenzb@gmail.com>
2024-04-30 12:38:33 -0700

    Added docs for keyword search route

--- src/routes/open/books.ts
+++ src/routes/open/books.ts
@@ -20,5 +20,27 @@ bookRouter.get('/all',
             res.status(500).send("Server Error, so sorry!")
         })
     })
+/*
+* @api {get} kwSearch
+*
+* @apiDescription Performs a keyword search of all books in the database by title and author.
+*
+* @apiName prefer getKwSearch
+* @apiGroup open
+*
+* @apiParam {string} q The keywords to search the database for.
+*
+* @apiSuccess (200) {Array<IBook>} returns an array containing all matching books
+* @apiSuccess (204) {String} The query was successfully run, but no books were found.
+*
+* @apiError (400: Bad Request) {String} Client provided no or malformed query parameter.
+* @apiError (418: I'm a teapot) {String} Client requested server to make coffee, but only tea is available.
+*
+*/
+bookRouter.get('/search',
+        (req: Request, res: Response, next: NextFunction) => {
+            console.log("Somebody tried to search!")
+            res.status(501).send();
+        });
 
 export {bookRouter}
