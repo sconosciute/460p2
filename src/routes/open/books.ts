f0525251804ab461c632c45de6908205ad67a8fd
Enrique Vargas <enriquev@uw.edu>
2024-05-03 19:35:08 -0700

    Created my own branch to not mess with others and added my documentation.

--- src/routes/open/books.ts
+++ src/routes/open/books.ts
@@ -43,4 +43,65 @@ bookRouter.get('/search',
             res.status(501).send();
         });
 
+
+/*
+* @api {post} /books Request to add a new book
+*
+* @apiDescription This api allows an authenticated user with the correct permission to add a new book in the database,
+* with all the required information.
+*
+* @apiName PostAddBook
+* @apiGroup books
+*
+* @apiBody {number} id The added book's id
+* @apiBody {number} isbn-13 An identifier of the book
+* @apiBody {string} authors The creator of the book can have more than one
+* @apiBody {number} publication year A number that shows when the book was published
+* @apiBody {string} original title Name the book was first given
+* @apiBody {string} title Another name for the book
+* @apiBody {number} average rating The average amount of rate from 1-5
+* @apiBody {number} ratings count total amount of time the book was rated
+* @apiBody {number} rating_1 The amount of times book was rated 1
+* @apiBody {number} rating_2 The amount of times book was rated 2
+* @apiBody {number} rating_3 The amount of times book was rated 3
+* @apiBody {number} rating_4 The amount of times book was rated 4
+* @apiBody {number} rating_5 The amount of times book was rated 5
+* @apiBody {string} image url The url of the image
+* @apiBody {string} small image url The url of the small image of the book
+*
+*
+*
+* @apiSuccess (201) {String} Success new book was added to the database
+*
+* @apiError (400: missing parameter) {String} message "Required information for new book is missing"
+* @apiError (403: unauthorized user) {String} message "You do not have access to add book"
+* @apiError (401: permission denied) {String} message "You do not have permission to add book"
+*/
+
+
+/*
+* @api {delete} /books Request to delete a book
+*
+* @apiDescription Deletes a single book or more from the database by ISBN, author and title containing
+*
+* @apiName DeleteBooks
+* @apiGroup books
+*
+* @apiParam {String} author The name of the authors of the books you want to delete
+* @apiParam {String} title containing  Titles of the books you want to delete
+* @apiParam {Number} isbn The isbn of the book to delete
+*
+* @apiSuccess (200) {String} Success book was deleted!
+*
+* @apiError (404) {String} message "No books found matching the criteria"
+* @apiError (401) {String} message "No permission to delete books"
+* @apiError (403) {String} message "Unauthorized user"
+*/
+
+
+
+
+
+
+
 export {bookRouter}
