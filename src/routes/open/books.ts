9080ace9837fbddbb14205ba95bd187a22059525
Megumi Invencion <minven@uw.edu>
2024-05-03 23:07:58 -0700

    Added doc for getAllBooksByPubDate, getBooksByAuthor, and getBooksByRating.

--- src/routes/open/books.ts
+++ src/routes/open/books.ts
@@ -43,4 +43,61 @@ bookRouter.get('/search',
             res.status(501).send();
         });
 
+/*
+* @api {get} /allBooksByPubDate
+*
+* @apiDescription Request to retrieve all books, sorted by publication date.
+*   If there are multiple books published on the same date, those books are then
+*   sorted in descending alphabetical order (A to Z).
+*
+* @apiName getAllBooksByPubDate
+* @apiGroup books
+*
+* @apiSuccess (200: Success) {Array<IBook>} Returns an array of all books with
+*   the specified author, if any.
+*
+* @apiError (500: Internal server error) {String} Server or database error occurred.
+*/
+// method goes here
+
+/*
+* @api {get} /author
+*
+* @apiDescription Request to search and retrieve all books with the specified
+*   author.
+*
+* @apiName getBooksByAuthor
+* @apiGroup books
+*
+* @apiBody {String} authorName The author's first and/or last name.
+*
+* @apiSuccess (200: Success) {Array<IBook>} Returns an array of all books sorted
+*   by publication date.
+*
+* @apiError (400: Bad request) {String} Missing parameter - Author name required.
+* @apiError (500: Internal server error) {String} Server or database error occurred.
+*/
+// method goes here
+
+/*
+* @api {get} /rating
+*
+* @apiDescription Request to search and retrieve all books that have a rating
+*   that is within the given rating range (inclusive).
+*
+* @apiName getBooksByRating
+* @apiGroup books
+*
+* @apiBody {int} min The minimum rating.
+* @apiBody {int} max The maximum rating.
+*
+* @apiSuccess (200: Success) {Array<IBook>} Returns an array of all books that
+*   have a rating within the given range.
+*
+* @apiError (400: Bad request) {String} Missing parameter(s) - Min and Max rating
+*   required.
+* @apiError (500: Internal server error) {String} Server or database error occurred.
+*/
+// method goes here
+
 export {bookRouter}
