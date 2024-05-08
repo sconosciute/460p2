c4e8083ee25db109d1ba3bb739bd8fae888a5fd3
Enrique Vargas <enriquev@uw.edu>
2024-05-08 13:56:34 -0700

    Fixed the api doc for delete

--- src/routes/open/books.ts
+++ src/routes/open/books.ts
@@ -57,7 +57,7 @@ bookRouter.get('/search',
 * @apiBody {number} isbn-13 An identifier of the book
 * @apiBody {string} authors The creator of the book can have more than one
 * @apiBody {number} publication year A number that shows when the book was published
-* @apiBody {string} original title Name the book was first given
+* @apiBody {string} original_title Name the book was first given
 * @apiBody {string} title Another name for the book
 * @apiBody {number} average rating The average amount of rate from 1-5
 * @apiBody {number} ratings count total amount of time the book was rated
@@ -66,8 +66,8 @@ bookRouter.get('/search',
 * @apiBody {number} rating_3 The amount of times book was rated 3
 * @apiBody {number} rating_4 The amount of times book was rated 4
 * @apiBody {number} rating_5 The amount of times book was rated 5
-* @apiBody {string} image url The url of the image
-* @apiBody {string} small image url The url of the small image of the book
+* @apiBody {string} image_url The url of the image
+* @apiBody {string} small_image_url The url of the small image of the book
 *
 *
 *
@@ -82,13 +82,12 @@ bookRouter.get('/search',
 /*
 * @api {delete} /books Request to delete a book
 *
-* @apiDescription Deletes a single book or more from the database by ISBN, author and title containing
+* @apiDescription Deletes a single book from the database by ID or the ISBN.
 *
 * @apiName DeleteBooks
 * @apiGroup books
 *
-* @apiParam {String} author The name of the authors of the books you want to delete
-* @apiParam {String} title containing  Titles of the books you want to delete
+* @apiParam {Number} id The primary key ID of the book to delete
 * @apiParam {Number} isbn The isbn of the book to delete
 *
 * @apiSuccess (200) {String} Success book was deleted!
@@ -98,6 +97,24 @@ bookRouter.get('/search',
 * @apiError (403) {String} message "Unauthorized user"
 */
 
+/*
+* @api {delete} /books Request to delete a range of books
+*
+* @apiDescription This deletes a range of books within a minimum book ID and maximum book ID
+* inclusively.
+*
+* @apiName DeleteRangeBooks
+* @apiGroup books
+*
+* @apiParam {Number} min_id The minimum ID of the range
+* @apiParam {Number} max_id the maximum ID of the range
+*
+* @apiSuccess (200) {String} Success: Range of books deleted!
+*
+* @apiError (404) {String} message "No books found matching the criteria"
+* @apiError (401) {String} message "No permission to delete books"
+* @apiError (403) {String} message "Unauthorized user"
+*/
 
 
 
