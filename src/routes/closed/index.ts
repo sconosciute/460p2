c86f9850a9c852946f8b8bdecd3e3550d90854a7
Megumi Invencion <minven@uw.edu>
2024-05-15 04:14:37 -0700

    Added books.ts to closed routes.

--- src/routes/closed/index.ts
+++ src/routes/closed/index.ts
@@ -2,9 +2,11 @@ import express, { Router } from 'express';
 
 import { checkToken } from '../../core/middleware';
 import { tokenTestRouter } from './tokenTest';
+import { bookRouter } from './books';
 
 const closedRoutes: Router = express.Router();
 
 closedRoutes.use('/jwt_test', checkToken, tokenTestRouter);
+closedRoutes.use('/books', bookRouter);
 
 export { closedRoutes };
