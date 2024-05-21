d4f9e84c69c53ffe5b8083807bfbf9e814ccd3b6
Zac <andersenzb@gmail.com>
2024-05-20 19:19:32 -0700

    Updated database migration system to automatically upgrade from stock all the way to V4

--- src/core/utilities/sql_conn.ts
+++ src/core/utilities/sql_conn.ts
@@ -29,47 +29,48 @@ const pool = new Pool(pgConfig);
 /**
  * Runs database up migrations from current detected version.
  */
-function migrate() {
-    let version: number;
-    pool.query('SELECT * FROM schema_version')
-        .then((res) => {
-            version = res.rows[0]?.version;
-            switch (version) {
-                case 1 :
-                    console.log('Upgrading DB Schema to V2');
-                    upgrade(up2Path);
-                    break;
-                case 2 :
-                    console.log('Upgrading DB Schema to V3');
-                    upgrade(up3Path);
-                    break;
-                case 3 :
-                    console.log('Upgrading to DB Schema V4');
-                    upgrade(up4Path);
-                    break;
-                case 4 :
-                    console.log('DB Schema V4 up to date');
-                    break;
-                default :
-                    throw new Error(`Unrecognized database schema version ${version}, panicking!`);
-            }
-        })
-        .catch((err) => {
-            //Err 42P01 = failed to find table
-            if (err.code == '42P01') {
-                upgrade(up1Path);
-            } else {
-                console.error(`Unable to upgrade database due to ${err}`);
-            }
-        });
+async function migrate() {
+    try {
+        const res = await pool.query('SELECT * FROM schema_version');
+        await migrateFromVersion(res.rows[0].version)
+    } catch (err) {
+        //Err 42P01 = failed to find table
+        if (err.code == '42P01') {
+            await migrateFromVersion(0);
+        } else {
+            console.error(`Unable to upgrade database due to ${err}`);
+        }
+    }
 }
 
-function upgrade(path: string) {
+async function migrateFromVersion(version: number) {
+    switch (version) {
+        case 0 :
+            console.log('Upgrading DB Schema to V1');
+            await upgrade(up1Path);
+        case 1 :
+            console.log('Upgrading DB Schema to V2');
+            await upgrade(up2Path);
+        case 2 :
+            console.log('Upgrading DB Schema to V3');
+            await upgrade(up3Path);
+        case 3 :
+            console.log('Upgrading to DB Schema V4');
+            await upgrade(up4Path);
+        case 4 :
+            console.log('DB Schema V4 up to date');
+            break;
+        default :
+            throw new Error(`Unrecognized database schema version ${version}, panicking!`);
+    }
+}
+
+async function upgrade(path: string) {
     try {
         const query = fs.readFileSync(path);
         console.log('Retrieved migration script, running upgrade');
-        pool.query(query.toString())
-            .then(() => console.log('Upgraded DB Schema Successfully'));
+        await pool.query(query.toString());
+        console.log('Upgraded DB Schema Successfully')
     } catch (err) {
         console.error(`Failed to upgrade DB schema due to ${err}`);
         throw err;
